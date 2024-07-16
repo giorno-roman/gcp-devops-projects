b89cfb9bdefd5f290b5d78083413358e56ddd0f8
giorno-romain <giorno_tientcheu@persistent.com>
2024-07-16 15:07:15 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import flask
-app = flask(__name__)
+from flask import Flask  # type: ignore
+
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, sample Flask application'
+def hello():
+    return "Hello, sample Flask application"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
