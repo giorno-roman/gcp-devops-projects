555cf0d2be7ccd3fba76a898a45841596e764f63
giorno-romain <giorno_tientcheu@persistent.com>
2024-07-26 09:29:21 -0500

    disable trunk bandit

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask  # type: ignore
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello, sample Flask application"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
